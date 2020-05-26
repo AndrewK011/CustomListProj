406b131b323a506a200a36993dec77e773605b20
Andrew K <drummer4life93@yahoo.com>
2020-05-26 17:58:53 -0500

    Added Add method with accompanying test methods.

--- CustomListProject/CustomList.cs
+++ CustomListProject/CustomList.cs
@@ -1,19 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CustomListProject
 {
     public class CustomList<T>
     {
-        private T[] items = new T[4];
+        private T[] items;
+        private T[] tempItems;
+        private int count;
+        private int capacity;
+        public int Count { get { return count; } set { count = value; } }
+        public int Capacity { get { return capacity; } set { capacity = value; } }
+
+        public CustomList()
+        {
+            capacity = 4;
+            count = 0;
+            items = new T[capacity];
+        }
+
+        public T this[int i]
+        {
+            get
+            {
+                return items[i];
+            }
+            set
+            {
+                items[i] = value;
+            }
+            
+        }
        
 
         public void Add(T item)
         {
-            items[0] = item;
+            if (Capacity == Count)
+            {
+                Capacity += Capacity;
+                tempItems = new T[Capacity];
+
+                for (int i = 0; i < Count; i++)
+                {
+                    tempItems[i] = items[i];
+                }
+
+                items = tempItems;          
+
+            }
+          
+                items[Count] = item;
+                Count++;
+          
+            // inserting into the array
+            //if count and capacity are equal
+            //then increase capacity
+            // copy values over to new array
+        }
+
+        public void Remove(T item)
+        {         
+            for (int i = 0; i < Count; i++)
+            {
+                if(items[i].Equals(item))
+                {
+
+                    Count--;
+                    break;
+                }
+            }
         }
     }
 }
--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -1,25 +1,168 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CustomListProject;
+using System.Collections.Generic;
+
 namespace UnitTestProject
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void Add_TwoPositiveValues_returnPositiveSum()
+        public void Add_IntegerToList_ReturnIntegerAdded()
         {
             CustomList<int> list = new CustomList<int>();
 
-            int value1 = 7;
-            int value2 = 11;
-            int expected = 18;
+            int value1 = 7;         
+            int expected = 7;
             int actual;
 
-            actual = list.Add(value1, value2);
+            list.Add(value1);
+
+            actual = list[0];
 
             Assert.AreEqual(expected, actual);
         }
+        [TestMethod]
+        public void Add_IntegerToList_CountIncreaseByOne()
+        {
+            CustomList<int> list = new CustomList<int>();
+
+            int value1 = 5;         
+            int expected = 1;
+
+            list.Add(value1);
+            int actual = list.Count;
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_FiveIntsToList_ReturnFifthIntAdded()
+        {
+            CustomList<int> list = new CustomList<int>();
+
+            int value1 = 1;
+            int value2 = 2;
+            int value3 = 3;
+            int value4 = 4;
+            int value5 = 5;
+            
+            
+            int expected = 5;
+            int actual;
+
+            list.Add(value1);
+            list.Add(value2);
+            list.Add(value3);
+            list.Add(value4);
+            list.Add(value5);
+
+            actual = list[4];
+
+            Assert.AreEqual(expected, actual);
+        }      
+
+        [TestMethod]
+        public void Add_DoubleToList_ReturnDoubleAdded()
+        {
+            CustomList<double> list = new CustomList<double>();
+
+            double value1 = 9.5;         
+            double expected = 9.5;
+            double actual;
+
+            list.Add(value1);
+
+            actual = list[0];
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_BoolToList_ReturnBoolAdded()
+        {
+            CustomList<bool> list = new CustomList<bool>();
+
+            bool value1 = true;         
+            bool expected = true;
+            bool actual;
+
+            list.Add(value1);
+
+            actual = list[0];
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_StringToList_ReturnStringAdded()
+        {
+            CustomList<string> list = new CustomList<string>();
+
+            string value1 = "This is a string.";
+            string expected = "This is a string.";
+            string actual;
+
+            list.Add(value1);
+
+            actual = list[0];
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]     
+        public void Remove_FirstIntFromList_SecondIntNowInFirstIndex()
+        {
+            CustomList<int> list = new CustomList<int>();
+
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+
+            list.Remove(1);
+
+            int expected = 2;
+            int  actual = list[0];
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]     
+        public void Remove_FirstIntFromList_CountDecreasesByOne()
+        {
+            CustomList<int> list = new CustomList<int>();
+
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+
+            list.Remove(1);
+
+            int expected = 4;
+            int  actual = list.Count;
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Remove_FirstTrueFromList_FirstIndexIsFalse()
+        {
+            CustomList<bool> list = new CustomList<bool>();
+
+            list.Add(true);
+            list.Add(false);
+            list.Add(true);
+            list.Add(false);
+            list.Add(true);
+
+            list.Remove(true);
+
+            bool expected = false;
+            
+            bool actual = list[0];
+
+            Assert.AreEqual(expected, actual);
+        }
+        
         
     }
 }
